65298c7e224c16eae699036da3143f7f7a25a2e8
Vananh Vo <vananhvo.0108@gmail.com>
2018-09-25 11:28:17 -0700

    change function name

--- jonathanC.cpp
+++ jonathanC.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
-#include <cstring>
-#include <X11/Xlib.h>
-#include <GL/glx.h>
 #include "log.h"
 #include "fonts.h"
 
-void show_credits(Rect x, int y)
+void vananhV(Rect x, int y)
 {
     ggprint8b(&x, y, 0x00ff0000, "Jonathan Crawford");
 }
