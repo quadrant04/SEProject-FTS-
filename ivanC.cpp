64748f3d3b698e2c4987226cf22219e1cb1126f9
Ivan <42749125+TheIllusiveDeveloper@users.noreply.github.com>
2018-09-25 10:49:26 -0700

    Update ivanC.cpp

--- ivanC.cpp
+++ ivanC.cpp
@@ -2,9 +2,13 @@
 // This file will show my name in the credits for our project
 
 #include <iostream>
+#include <fonts.h>
 using namespace std;
 
-void show_credits()
+void show_credits(int x, int y)
 {
-    cout << "Ivan Cisneros";
+    r.bot = y;
+    r.left = x;
+    r.center = 0;
+    ggprint8b(&r, 16, 0x00ff0000, "Ivan Cisneros");
 }
