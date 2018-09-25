7762d458ac61015b5e38bacc3e789b7559d02fd8
Vananh Vo <vananhvo.0108@gmail.com>
2018-09-25 11:13:52 -0700

    Included some new libraries

--- vananhV.cpp
+++ vananhV.cpp
@@ -3,14 +3,14 @@
 //Description: This file will include how to display backgrounds and sprites into the game
 
 #include <iostream>
+#include <cstring>
+#include <X11/Xlib.h>
 #include <GL/glx.h>
-#include <font.h>
+#include "log.h"
+#include "fonts.h"
 using namespace std;
 
-void showVananhName(int x, int y)
+void showVananhName(Rect x, int y)
 {	
-	r.bot = y;
-	r.left = x;
-	r.center = 0;
-	ggprint8b (&r, 16, 0x00ff0000, "Art Design: Vananh Vo \n");
+	ggprint8b (&x, y, 0x00ff0000, "Art Design: Vananh Vo \n");
 }
