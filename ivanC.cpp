179bf025601ce4c001f65c629c1923a5b6fe8402
Ivan <42749125+TheIllusiveDeveloper@users.noreply.github.com>
2018-09-25 10:33:23 -0700

    Add files via upload
    This will show my name in the credits.

---
+++ ivanC.cpp
@@ -0,0 +1,10 @@
+// Author: Ivan Cisneros
+// This file will show my name in the credits for our project
+
+#include <iostream>
+using namespace std;
+
+void show_credits()
+{
+    cout << "Ivan Cisneros";
+}
