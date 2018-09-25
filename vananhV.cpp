006c54c39c48528f5294b781a4cb762f0bb356e1
Vananh Vo <vananhvo.0108@gmail.com>
2018-09-25 11:30:47 -0700

    Update vananhV.cpp

--- vananhV.cpp
+++ vananhV.cpp
@@ -3,14 +3,11 @@
 //Description: This file will include how to display backgrounds and sprites into the game
 
 #include <iostream>
-#include <cstring>
-#include <X11/Xlib.h>
-#include <GL/glx.h>
 #include "log.h"
 #include "fonts.h"
 using namespace std;
 
-void showVananhName(Rect x, int y)
+void vananhV(Rect x, int y)
 {	
 	ggprint8b (&x, y, 0x00ff0000, "Art Design: Vananh Vo \n");
 }
