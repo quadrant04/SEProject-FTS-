a7fd0674505788ef0a683e4a1217f0337f864197
Vananh Vo <vananhvo.0108@gmail.com>
2018-09-25 10:44:31 -0700

    Update vananhV.cpp
    Deleted cout, using ggprint8b instead
    Include font.h 
    Include GL/glx.h

--- vananhV.cpp
+++ vananhV.cpp
@@ -3,10 +3,14 @@
 //Description: This file will include how to display backgrounds and sprites into the game
 
 #include <iostream>
+#include <GL/glx.h>
+#include <font.h>
 using namespace std;
 
 void showVananhName()
-{
-	cout << "Vananh Vo ";
-	cout endl;
+{	
+	r.bot =gl.yres - 20;
+	r.left = 10;
+	r.center = 0;
+	ggprint8b (&r, 16, 0x00ff0000, "Art Design: Vananh Vo \n");
 }
