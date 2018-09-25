35d5f5dff9cff0d1a354ff2559305ed2e1801c37
Ivan <42749125+TheIllusiveDeveloper@users.noreply.github.com>
2018-09-25 11:26:26 -0700

    Update ivanC.cpp
    Changed for object rect

--- ivanC.cpp
+++ ivanC.cpp
@@ -5,11 +5,10 @@
 #include "fonts.h"
 using namespace std;
 
-void show_credits(int x, int y)
+void show_credits(Rect x, int y)
 {
-    Rect r;
-    r.bot = y;
-    r.left = x;
-    r.center = 0;
-    ggprint8b(&r, 16, 0x00ff0000, "Ivan Cisneros");
+    x.bot = y;
+    x.left = 10;
+    x.center = 0;
+    ggprint8b(&x, 16, 0x00ff0000, "Ivan Cisneros");
 }
