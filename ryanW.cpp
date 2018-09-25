b560851003f2c4d0ce9e448a60e5984c321b4710
Ryan Wallace <rywallace22@gmail.com>
2018-09-25 11:28:38 -0700

    Add files via upload

--- ryanW.cpp
+++ ryanW.cpp
@@ -8,11 +8,10 @@
 #include <iostream>
 using namespace std; 
 
-void show_credits(int x, int y) {
-    Rect r;
-    r.bot = y;
-    r.left = x;
-    r.center = 0;
+void ryanW(Rect x, int y) {
+    x.bot = y;
+    x.left = 10;
+    x.center = 0;
     ggprint8b(&r, 16, 0x00ff0000, "Ryan Wallace");
 }
 
