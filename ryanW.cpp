bee272148549680bcfa4ea3a0fd78df45f386521
Ryan Wallace <rywallace22@gmail.com>
2018-09-25 10:58:45 -0700

    added the ability to print onto game screen

---
+++ ryanW.cpp
@@ -0,0 +1,19 @@
+//Author: Ryan Wallace
+//
+//Prints out the names of one member
+//
+
+
+#include <fonts.h>
+#include <iostream>
+using namespace std; 
+
+Rect r
+void show_credits(int x, int y) {
+    r.bot = y;
+    r.left = x;
+    r.center = 0;
+    ggprint8b(&r, 16, 0x00ff0000, "Ryan Wallace");
+}
+
+
