94b293414d206c4afa2c50f69284f3bd49df5592
Ryan Wallace <rywallace22@gmail.com>
2018-09-25 11:29:18 -0700

    Add files via upload

--- ryanW.cpp
+++ ryanW.cpp
@@ -12,7 +12,7 @@ void ryanW(Rect x, int y) {
     x.bot = y;
     x.left = 10;
     x.center = 0;
-    ggprint8b(&r, 16, 0x00ff0000, "Ryan Wallace");
+    ggprint8b(&x, 16, 0x00ff0000, "Ryan Wallace");
 }
 
 
