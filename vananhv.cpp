11db9fa4c931d57fe5972a8b486e8d74a2ec9799
Vananh Vo <vananhvo.0108@gmail.com>
2018-09-25 10:20:25 -0700

    Adding vananhv.cpp file into the repo
    My first source file commit

---
+++ vananhv.cpp
@@ -0,0 +1,11 @@
+//Author: Vananh Vo
+//Date: 09/25/2018
+//Description: This file will include how to display backgrounds and sprites into the game
+
+#include <iostream>
+using namespace std;
+
+void printName()
+{
+	cout << "Vananh Vo>;
+}
