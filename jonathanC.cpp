d7e0e883bae3894cc3644787105c5bc4fdba1394
Student <student@lab-240-016.cs.csubak.edu>
2018-10-03 10:43:13 -0700

    Updated info section

--- jonathanC.cpp
+++ jonathanC.cpp
@@ -1,6 +1,9 @@
 //Author: Jonathan Crawford
+//
+//two functions. One displays the authors name when called using openGl.
+//the second, displays a picture representing the author when called.
 
-#include <iostream>
+
 #include "fonts.h"
 #include <GL/glx.h>
 #include <math.h>
@@ -12,24 +15,26 @@ void jonathanC(Rect x, int y)
 
 void showJonathanPicture(int x, int y, GLuint textid)
 {
-glColor3ub(255,255,255);
-/*
-        static float angle = 0.0f;
-        float fx = (float)x;
-        float fy = (float)y;
-        float a += sin(angle) * 10.0f;
-        fx += a;
-        angle += 0.2f;
-        */
-        int wid=40;
-        glPushMatrix();
-        glTranslatef(x,y,0);
-        glBindTexture(GL_TEXTURE_2D, textid);
-        glBegin(GL_QUADS);
-				glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid,-wid);
-				glTexCoord2f(0.0f, 0.0f); glVertex2i(-wid, wid);
-				glTexCoord2f(1.0f, 0.0f); glVertex2i( wid, wid);
-				glTexCoord2f(1.0f, 1.0f); glVertex2i( wid,-wid);
-		glEnd();
-		glPopMatrix();
+    glColor3ub(255,255,255);
+    /*
+    static float angle = 0.0f;
+    float fx = (float)x;
+    float fy = (float)y;
+    float a += sin(angle) * 10.0f;
+    fx += a;
+    angle += 0.2f;
+    */
+    int wid=40;
+    glPushMatrix();
+    glTranslatef(x,y,0);
+    glBindTexture(GL_TEXTURE_2D, textid);
+    
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid,-wid);
+    glTexCoord2f(0.0f, 0.0f); glVertex2i(-wid, wid);
+    glTexCoord2f(1.0f, 0.0f); glVertex2i( wid, wid);
+    glTexCoord2f(1.0f, 1.0f); glVertex2i( wid,-wid);
+    
+    glEnd();
+    glPopMatrix();
 }
