3d886b7281d7d5ab840b52d54c514c06195121a1
Ryan Wallace <rywallace22@gmail.com>
2018-11-27 10:24:50 -0800

    Update ryanW.cpp

--- ryanW.cpp
+++ ryanW.cpp
@@ -27,7 +27,7 @@ typedef float Vec[3];
 //	float x, y, dist;
 //};
 
-Image unitList[1] = {"./images/ob.jpeg"};
+Image TowerList[1] = {"./images/ob.jpeg"};
 extern X11_wrapper x11;
 //extern void init_unit(Unit*);
 extern void displayTowers(float x, float y, GLuint texid);
