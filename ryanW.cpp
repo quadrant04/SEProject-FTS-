c22f87e026b60b40065c2c5f474423f542a658eb
Ryan Wallace <rywallace22@gmail.com>
2018-11-27 09:59:32 -0800

    Add files via upload

--- ryanW.cpp
+++ ryanW.cpp
@@ -3,12 +3,16 @@
 //Prints out the names of one member
 //
 
-
+// Start of all standardized items
 #include "fonts.h"
 #include <iostream>
 #include <GL/glx.h>
 #include <math.h>
 using namespace std; 
+
+Image towerList[1] = {"./images/ob.jpg"};
+
+// End of all standardized items
 
 void ryanW(Rect x, int y) 
 {
@@ -35,3 +39,96 @@ void showRyanPicture(int x, int y, GLuint texid)
      glEnd();
      glPopMatrix();
 }
+
+// Tower Object creation
+
+class Tower {
+public:
+	int onoff;
+	int frame;
+	Vec pos;
+	Image *image;
+	GLuint tex;
+	struct timespec time;
+	Tower() {
+		onoff = 0;
+		frame = 0;
+		image = NULL;
+	}
+};
+
+// Tower Items
+const int MAX_TOWERS = 10;
+static int numTowers = 0;
+static Tower basicTower[MAX_TOWERS];
+
+class level {
+public:
+	Vec spawn[1];
+	level() {
+		spawn = {0};
+	}
+}level;
+
+void setSpawn(Flt x, Flt y, Flt z) 
+{
+	level.spawn = {{x}, {y}, {z}};
+};
+
+
+// Basic Tower Creation
+void createTower(int i, int x, int y)
+{	
+	Tower *p; 
+	p = &basicTower[numTowers];
+	int w, h;
+	w = towerList[i].width;
+	h = towerList[i].height;
+	//setup image for the unit.
+	glGenTextures(1, &p->tex);
+	glBindTexture(GL_TEXTURE_2D, p->tex);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, 3, w, h, 0,
+		         GL_RGB, GL_UNSIGNED_BYTE, towerList[i].data);
+
+numTowers++;
+
+// Displays Towers onto the screen
+void displayTowers() {
+    if (numTowers < 1)
+        return;
+    Tower *p;
+    for (int i = 0; i < numTowers; i++) {
+        p = &basicTower[i];
+        float x = p->pos[0];
+        float y = p->pos[1];
+	    static int wid = 30;
+	    glColor3ub(255,255,255);
+        glPushMatrix();
+	    
+	    glBindTexture(GL_TEXTURE_2D, p->tex);
+	    glTranslated(x, y, 0);
+	    glBegin(GL_QUADS);
+	    glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid,-wid);
+	    glTexCoord2f(0.0f, 0.0f); glVertex2i(-wid, wid);
+	    glTexCoord2f(1.0f, 0.0f); glVertex2i( wid, wid);
+	    glTexCoord2f(1.0f, 1.0f); glVertex2i( wid,-wid);
+        glEnd();
+        
+	    glPopMatrix();
+	}
+	glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
