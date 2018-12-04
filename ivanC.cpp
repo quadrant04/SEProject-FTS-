5088d2b330fe232111439414c5d501212949d15c
Ivan <42749125+TheIllusiveDeveloper@users.noreply.github.com>
2018-12-03 22:18:26 -0800

    Display menu options

--- ivanC.cpp
+++ ivanC.cpp
@@ -1,12 +1,42 @@
 // Author: Ivan Cisneros
 // This file will show my name in the credits for our project
+// -Show my name in the credits for our project (Completed date)
+// -Display button functionality
+//  Edit:  Display options (11/27/2018)
+//         12/01/2018 - Displaying menu bar
+//
 
 #include <iostream>
 #include "fonts.h"
 #include <GL/glx.h>
 #include <math.h>
-using namespace std;
+#include <stdlib.h> /* malloc */
+#include <X11/keysym.h>
+#include <X11/Xlib.h>
+#include "image.h" 
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#ifdef USE_OPENAL_SOUND
+#include </usr/include/AL/alut.h>
+#endif //USE_OPENAL_SOUND
 
+//Images that will be displayed for toggling sound and showing win/loss
+/*
+Image icons[3] = {"./images/time.png", "./images/prize.png", 
+	          "./images/rip.png"};
+*/
+//------------------------------------------------------------------------------
+
+//------------ Extern functions for slime and point # --------------------------
+extern int getPointCount();
+extern int getSlimeCount();
+extern X11_wrapper x11;
+//------------------------------------------------------------------------------
+
+//---------------------Ivan's credit functions----------------------------------
 void ivanC(Rect x, int y)
 {
   ggprint8b(&x, 16, 0x00ff0000, "Ivan Cisneros");
@@ -35,39 +65,193 @@ void showIvanPicture(int x, int y, GLuint textid)
   glEnd();
   glPopMatrix();
 }
+//------------------------------------------------------------------------------
 
-void showButtonOptions(Rect x, int y) {
-	//unsigned int c = 0x00ffff44;
-	x.left = 1000;
-	x.center = 0;
-
-        ggprint8b(&x, 16, 0x00ff0000, "U - Spawn Unit (Toggle on or off)");
-        //ggprint8b(&x, 16, 0x00ff0000, "1 - Toggle Sound");
-        ggprint8b(&x, 16, 0x00ff0000, "C - Credits");
-        ggprint8b(&x, 16, 0x00ff0000, "R - Reset pathing of slimes");
-        ggprint8b(&x, 16, 0x00ff0000, "P - Pathing");
-}
-
-void showCount(Rect x, int y)
+//------------------------Button options----------------------------------------
+void boxMenu(int x)
 {
-	x.left = 16;
-	x.center = 0;
-        ggprint8b(&x, 16, 0x00ff0000, "Slimes: ");
-        ggprint8b(&x, 16, 0x00ff0000, "Pathing #: ");
-        ggprint8b(&x, 16, 0x00ff0000, "");
-        ggprint8b(&x, 16, 0x00ff0000, "Press M for options ");
-
+  glPushMatrix();
+  glTranslatef(x, 375.0f, 0.0f);
+  glColor3f(0.1f, 0.6f, 0.1f);
+  glBegin(GL_QUADS);
+    glVertex2f(240.0, 290.0);
+    glVertex2f(-4.0, 290.0);
+    glVertex2f(-4.0, 510.0);
+    glVertex2f(240.0, 510.0);
+  glEnd();
+  glPopMatrix();
 }
-/*
-Pathing mode on/off
-//global,
-spawn unit on/off
-# of slimes //
-# of points // npoint
-//create function to get # of slimes, points,
-//modify to take two ints for show buttons
-extern int getSlimeCount
-extern int getPointCount
- -add to main cpp
-sprintf
-*/
+void boxPath(int x)
+{
+  glPushMatrix();
+  glTranslatef(x, 145.0f, 0.0f);
+    glColor3f(0.1f, 0.6f, 0.1f);
+  glBegin(GL_QUADS);
+    glVertex2f(237.0, 360.0);
+    glVertex2f(-4.0, 360.0);
+    glVertex2f(-4.0, 500.0);
+    glVertex2f(237.0, 500.0);
+  glEnd();
+  glPopMatrix();
+}
+
+void showButtonOptions(Rect x, int xres, int slimeToggle, int pathing) {
+  int color = 0xffffff;
+  char slimeONOFF[30];
+  char pathingONOFF[30]; 	
+  char slimeCount[30];
+  char pointCount[30];
+  int pCount = getPointCount(); 
+  
+  x.left = xres - 300;
+  boxMenu(x.left);
+
+  if (slimeToggle) {
+    sprintf(slimeONOFF, "U - Slime: ON ");
+
+  } else {
+    sprintf(slimeONOFF, "U - Slime: OFF ");
+		}
+  if (pathing) {
+    sprintf(pathingONOFF, "P - Pathing: ON ");
+
+  } else {
+    sprintf(pathingONOFF, "P - Pathing: OFF ");
+		}
+  
+  sprintf(slimeCount, "# of Slimes: %d", getSlimeCount());
+  sprintf(pointCount, "# of Points: %d", pCount);
+  	
+  ggprint16(&x, 26, color, "");
+  ggprint16(&x, 26, color, slimeONOFF);
+  ggprint16(&x, 26, color, slimeCount);
+  ggprint16(&x, 26, color, "^ - Increase Slime Speed");
+  ggprint16(&x, 26, color, "v - Decrease Slime Speed");
+  ggprint16(&x, 26, color, "R - Reset pathing of slimes");
+  ggprint16(&x, 26, color, "Q - Title Screen");
+  ggprint16(&x, 26, color, "C - Credits");
+  ggprint16(&x, 26, color, pathingONOFF);
+  
+  //ggprint8b(&x, 16, 0x00ff0000, "1 - Toggle Sound");
+  
+  if (pathing) {
+    boxPath(x.left);
+    ggprint16(&x, 26, color, "");
+    ggprint16(&x, 26, color, pointCount);
+    ggprint16(&x, 26, color, "Right Click To Add");
+    ggprint16(&x, 26, color, "S - Show Chords");
+    ggprint16(&x, 26, color, "T - Towers");
+    
+    if (pCount > 0) {
+      ggprint16(&x, 26, color, "Initial spawn point: ON");
+    } else {
+      ggprint16(&x, 26, color, "Initial spawn point: OFF");
+    }
+  }
+}
+
+//------------------------------------------------------------------------------
+
+//----------------------Display # of slimes and points--------------------------
+void showMenu(Rect x, int y)
+{
+  int BLACK = 0x000000;
+  int RED = 0xff0000;
+	x.left = 1;
+	x.center = 0;
+
+  ggprint13(&x, 16, BLACK, "Press M For Options ");
+  ggprint13(&x, 16, BLACK, "");
+  ggprint13(&x, 16, BLACK, "Press Esc to Exit Game");
+  ggprint13(&x, 16, RED, "And Doom The Slimes! ");
+}
+
+//------------------------------------------------------------------------------
+
+//---------------------------Sound functions------------------------------------
+void* introMusic(void* arg)
+{
+  if (arg) {
+
+  }
+
+  #ifdef USE_OPENAL_SOUND
+  alutInit(0, NULL);
+  if (alGetError() != AL_NO_ERROR) {
+    printf("ERROR: alutInit()\n");
+    return 0;
+  }
+  //Clear error state.
+  alGetError();
+  //Setup the listener.
+  //Forward and up vectors are used.
+  float vec[6] = {0.0f,0.0f,1.0f, 0.0f,1.0f,0.0f};
+  alListener3f(AL_POSITION, 0.0f, 0.0f, 0.0f);
+  alListenerfv(AL_ORIENTATION, vec);
+  alListenerf(AL_GAIN, 1.0f);
+  
+  //Buffer holds the sound information.
+  ALuint alBuffer;
+  alBuffer = alutCreateBufferFromFile("sounds/ftsintro.wav");
+  //Source refers to the sound.
+  ALuint alSource;
+  //Generate a source, and store it in a buffer.
+  alGenSources(1, &alSource);
+  alSourcei(alSource, AL_BUFFER, alBuffer);
+  //Set volume and pitch to normal, no looping of sound.
+  alSourcef(alSource, AL_GAIN, 1.0f);
+  alSourcef(alSource, AL_PITCH, 1.0f);
+  alSourcei(alSource, AL_LOOPING, AL_FALSE);
+  
+  if (alGetError() != AL_NO_ERROR) {
+    printf("ERROR: setting source\n");
+    alutExit();
+    pthread_exit(NULL);
+  }
+
+  #endif
+  
+  pthread_exit(NULL);
+}
+
+void* levelMusic(void* arg)
+{
+  #ifdef USE_OPENAL_SOUND
+    alutInit(0, NULL);
+	  if (alGetError() != AL_NO_ERROR) {
+		  printf("ERROR: alutInit()\n");
+		  return 0;
+	  }
+	  //Clear error state.
+	  alGetError();
+	//Setup the listener.
+	//Forward and up vectors are used.
+	float vec[6] = {0.0f,0.0f,1.0f, 0.0f,1.0f,0.0f};
+	alListener3f(AL_POSITION, 0.0f, 0.0f, 0.0f);
+	alListenerfv(AL_ORIENTATION, vec);
+	alListenerf(AL_GAIN, 1.0f);
+	//
+	//Buffer holds the sound information.
+	ALuint alBuffer;
+	alBuffer = alutCreateBufferFromFile("sounds/level1.wav");
+	//Source refers to the sound.
+	ALuint alSource;
+	//Generate a source, and store it in a buffer.
+	alGenSources(1, &alSource);
+	alSourcei(alSource, AL_BUFFER, alBuffer);
+	//Set volume and pitch to normal, no looping of sound.
+	alSourcef(alSource, AL_GAIN, 1.0f);
+	alSourcef(alSource, AL_PITCH, 1.0f);
+	alSourcei(alSource, AL_LOOPING, AL_FALSE);
+	if (alGetError() != AL_NO_ERROR) {
+		printf("ERROR: setting source\n");
+		alutExit();
+		//   inUse[i] = 0;
+		pthread_exit(NULL);
+		//return 0;
+  }
+
+  #endif
+  
+  pthread_exit(NULL);
+} 
